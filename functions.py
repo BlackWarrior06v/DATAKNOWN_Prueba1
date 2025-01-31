cdd5d683530d5f75e5b39da7a613cbfe785e064d
BlackWarrior06v <curipch2@gmail.com>
2025-01-30 21:17:35 -0600

    The total price is estimated

--- functions.py
+++ functions.py
@@ -12,3 +12,15 @@ def load_csv_data(path, separation, decimal, reversed):
 
 def fit_X(dates,a,b):
     return a + b*dates
+
+
+
+# The equipment price calculation per month will be the follows:
+# We will suppose that the percentajes given in the problem description 
+# are the percentaje of a material unity we need to buy
+
+def Equipment1_month_calculation(X,Y):
+    return 0.2 * X + 0.8 * Y
+
+def Equipment2_month_calculation(X,Y,Z):
+    return 0.33*X + 0.33*Y + 0.33*Z
